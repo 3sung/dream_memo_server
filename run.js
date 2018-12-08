5753311ec2b425bff93d35c920ecba444594f8b0
MyeongjiHan <41604634+MyeongjiHan@users.noreply.github.com>
2018-12-08 19:41:18 +0900

    회원관리

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const database = require('./database');
-
 const user = require('./manage/user');
+
 
 app.post('/signUp', (req, res)=> {
     console.log(req);
@@ -9,13 +9,75 @@ app.post('/signUp', (req, res)=> {
         user.signUp(connection,
             (rows)=>{
             req.send("success")
-                connection.release()
+            connection.release();
             },
             (err)=>{
             req.send("fail")
-
+            connection.release();
             }, req.body.userID, req.body.userEmail, req.body.userPW)
     });
+});
+
+app.get('/signIn', (req, res)=> {
+    console.log(req);
+    database.connect(function (connection){
+        user.signIn(connection,
+        (rows)=>{
+        res.json({
+            token: user.getToken(req.body.userID)
+        });
+        req.send("success")
+        connection.release();
+        },
+        (err)=>{
+        req.send("fail")
+        connection.release();
+        }, req.body.userID, req.body.userPW)
+    });
+});
+
+app.post('/edit', (req, res)=> {
+    console.log(req);
+    database.connect(function (connection){
+        user.signIn(connection,
+        (rows)=>{
+        req.send("success")
+        connection.release();
+        },
+        (err)=>{
+        req.send("fail")
+        connection.release();
+        }, req.body.userID, req.body.userPW, req.body.userNewPW)
+    });
+});
+
+app.post('/delete', (req, res)=> {
+    console.log(req);
+    database.connect(function (connection){
+        user.deleteID(connection,
+        (rows)=>{
+        req.send("success")
+        connection.release();
+        },
+        (err)=>{
+        req.send("fail")
+        connection.release();
+        }, req.body.userID, req.body.userPW)
+    });
+});
+
+
+// database.connect(function (connection) {
+//     connection.query(
+//         "SELECT * FROM DREAMMEMO_DB.User_TB;",
+//         function (err, rows, fields) {
+//             console.log(rows)
+//         })
+// });
+
+app.get('/', (req, res)=> {
+    console.log('접속 탐지')
+    res.send('success')
 });
 
 app.listen(80, () => {
